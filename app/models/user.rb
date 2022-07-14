49e44133184da674a0e85bb91fda5b62ef47ba46
victorHSFerraz <victorf178@gmail.com>
2022-07-13 22:21:31 -0300

    twitter create account callback

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    has_many :twitter_accounts
+
     has_secure_password
 
     validates :email, presence: true, format: { with: /\A[^@\s]+@[^@\s]+\z/, message: "Must be a valid email address"}
